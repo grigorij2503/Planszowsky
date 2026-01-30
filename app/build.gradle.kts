98fc693f05dc19231898e8c6ae92b513876ec6ef
Grog <g.kolaski@gmail.com>
2026-01-30 22:04:05 +0100

    Target sdk update, library updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.planszowsky.android"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.planszowsky.android"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
