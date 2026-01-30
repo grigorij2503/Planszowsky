0b8163e0500c6777b664aa9de5dfa42f4387c90c
Grog <g.kolaski@gmail.com>
2026-01-30 22:12:43 +0100

    Compose update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.core)
+    implementation(libs.androidx.material.icons.extended)
     
     // Navigation
     implementation(libs.androidx.navigation.compose)
